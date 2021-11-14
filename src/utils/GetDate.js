0c3305df84ca90ce967295f0dfa32571fe007dd2
eder <edra1791@gmail.com>
2021-11-13 22:55:23 -0500

    add reservation to API

--- src/utils/GetDate.js
+++ src/utils/GetDate.js
@@ -9,3 +9,15 @@ export function GetDate(separator = "-") {
     month < 10 ? `0${month}` : `${month}`
   }${separator}${date}`;
 }
+
+
+
+export function GetDateFormat(myCurrentDate, separator = "/") {
+  let date = myCurrentDate.getDate();
+  let month = myCurrentDate.getMonth() + 1;
+  let year = myCurrentDate.getFullYear();
+
+  return `${year}${separator}${
+    month < 10 ? `0${month}` : `${month}`
+  }${separator}${date}`;
+}
